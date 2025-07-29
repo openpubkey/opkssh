ea7e453628199637830a8479d7b2cfc9e6b2182e
Aaron <akl@riact.eu>
2025-07-29 19:48:20 +0200

    add deny_users to ServerConfig

--- commands/config/server_config.go
+++ commands/config/server_config.go
@@ -24,6 +24,7 @@ import (
 
 type ServerConfig struct {
 	EnvVars map[string]string `yaml:"env_vars"`
+	DenyUsers []string        `yaml:"deny_users"`
 }
 
 func NewServerConfig(c []byte) (*ServerConfig, error) {
