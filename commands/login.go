0869ab9cd54781ef17af1b93239e79e314c40f4c
Ethan Heilman <ethan.r.heilman@gmail.com>
2026-08-23 21:27:13 -0400

    Fixes bug in how refreshed token was being parsed

--- commands/login.go
+++ commands/login.go
@@ -622,6 +622,7 @@ func (l *LoginCmd) LoginWithRefresh(ctx context.Context, provider providers.Refr
 			// Sleep until a minute before expiration to give us time to refresh
 			// the token and minimize any interruptions
 			untilExpired := time.Until(time.Unix(claims.Expiration, 0)) - time.Minute
+			// untilExpired := time.Until(time.Now().Add(15 * time.Second))
 			log.Printf("Waiting for %v before attempting to refresh id_token...", untilExpired)
 			select {
 			case <-time.After(untilExpired):
@@ -1017,8 +1018,8 @@ func gitlabCiIssuer() string {
 }
 
 // payloadFromCompactPkt extracts the payload from a compact PK Token which
-// is always the second part of the '.' separated string.
+// is always the third part of the '.' separated string.
 func payloadFromCompactPkt(compactPkt []byte) []byte {
 	parts := bytes.Split(compactPkt, []byte("."))
-	return parts[1]
+	return parts[2]
 }
